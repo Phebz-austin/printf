461272e6808bb3959ef597638e4636449a33841e
Ollors-delights <ollorsdelight@gmail.com>
2023-05-30 01:34:25 -0700

    let us print

--- get_size.c
+++ get_size.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * get_size - Calculates size to cast the argument
- * @format: Formatted string in which to print arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * get_size - funtion calculates the size to cast an argument
+ * @format: Formatted string in which arguments are printed
+ * @i: integers
+ * Return: size
  */
 int get_size(const char *format, int *i)
 {
